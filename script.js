53af63fe5df761f70e115d39f7ce0accb0bea44a
mohitgaigan <mohitgaigan@gmail.com>
2026-01-28 23:38:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ const reveal = () => {
 };
 window.addEventListener("scroll", reveal);
 reveal();
+// Gallery lightbox
+const galleryImages = document.querySelectorAll(".gallery img");
+const lightbox = document.createElement("div");
+lightbox.className = "lightbox";
+document.body.appendChild(lightbox);
+
+const lightboxImg = document.createElement("img");
+lightbox.appendChild(lightboxImg);
+
+galleryImages.forEach(img => {
+  img.addEventListener("click", () => {
+    lightboxImg.src = img.src;
+    lightbox.classList.add("show");
+  });
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.classList.remove("show");
+});
