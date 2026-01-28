8745cd44dea9d276455ab517a577dde627cf0cb3
mohitgaigan <mohitgaigan@gmail.com>
2026-01-28 17:50:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggle = document.getElementById("toggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  localStorage.setItem("theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
+});
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
